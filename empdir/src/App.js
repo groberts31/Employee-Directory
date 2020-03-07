862fd812e9812177c4f76e7f31f6998dff8c4509
Greg Roberts <groberts3104@gmail.com>
2020-03-07 15:44:42 -0500

    added Navbar, Wrapper and Footer to import components

--- empdir/src/App.js
+++ empdir/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+
 
 function App() {
   return (
@@ -7,7 +11,7 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-        
+
         </Wrapper>
         <Footer />
       </div>

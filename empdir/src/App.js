745e940857aaf2b85635465c4501c220cc114fe6
Greg Roberts <groberts3104@gmail.com>
2020-03-07 17:06:44 -0500

    imported Employees.json to App.js and named in Employees

--- empdir/src/App.js
+++ empdir/src/App.js
@@ -2,17 +2,16 @@ import React from "react";
 import PageWorking from "./components/Page";
 import EmployeeInfo from "./components/EmployeeInfo";
 import Wrapper from "./components/Wrapper";
+import Employees from "./employees.json";
 
 
 
 function App() {
   return 
-  <div>
   <Wrapper>
     <PageWorking />
   <EmployeeInfo />
   </Wrapper>
-  </div>;
 }
 
 export default App;

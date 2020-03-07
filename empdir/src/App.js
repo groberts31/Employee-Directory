63da4092e84e59e05e14cfd77d474b2e6f7fdf50
Greg Roberts <groberts3104@gmail.com>
2020-03-07 17:38:54 -0500

    added Search folder with index.js and style.css in it and redid function for App.js to show employees

--- empdir/src/App.js
+++ empdir/src/App.js
@@ -1,17 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import PageWorking from "./components/Page";
-import EmployeeInfo from "./components/EmployeeInfo";
+import EmployeeInfo from "./components/Card";
 import Wrapper from "./components/Wrapper";
-import Employees from "./employees.json";
+import employees from "./employees.json";
 
+class App extends Component {
 
-
-function App() {
-  return 
-  <Wrapper>
-    <PageWorking />
-  <EmployeeInfo />
-  </Wrapper>
-}
-
-export default App;
+    state = {
+      search: "",
+      employees,
+      order: "asc"
+    };
+  
+    handleInputChange = event => {
+      console.log(event.target.value);
+      this.setState({ search: event.target.value });
+    };
+  
+    sortName = event => {
+      let currentEmployees = this.state.employees
+      .sort(function(a, b) {
+        if(a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+        if(a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+        return 0;
+      })
+      this.setState({ employees: currentEmployees });
+    }
+    
+    render() {
+      const filteredEmployees = this.state.employees.filter((employee) => { 
+        return employee.name.indexOf(this.state.search) !== -1;
+      })
+  
+      return (
+        <Wrapper>
+          <PageWorking />
+          <Search
+            handleInputChange={this.handleInputChange}
+            sortName={this.sortName}
+            search={this.state.search}
+          >
+          </Search>
+          {filteredEmployees.map(employee => (
+            <EmployeeInfo
+              id={employee.id}
+              key={employee.id}
+              name={employee.name}
+              image={employee.image}
+              role={employee.role}
+              contact={employee.contact}
+            />
+          ))}
+        </Wrapper>
+      );
+    }
+  }
+  
+  export default App;

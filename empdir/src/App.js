0af7f0d5705c679984ac54ee9d98c4816eeb5edc
Greg Roberts <groberts3104@gmail.com>
2020-03-07 16:56:29 -0500

    imported Wrapper and added it to App function

--- empdir/src/App.js
+++ empdir/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import PageWorking from "./components/Page";
 import EmployeeInfo from "./components/EmployeeInfo";
+import Wrapper from "./components/Wrapper";
 
 
 
 function App() {
   return 
   <div>
+  <Wrapper>
     <PageWorking />
   <EmployeeInfo />
+  </Wrapper>
   </div>;
 }
 
